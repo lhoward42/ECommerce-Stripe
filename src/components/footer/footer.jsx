4c364ae450437b2649e1d591488e335ada915dd2
Laurice Howard <lowkeylaurice@gmail.com>
2022-06-07 21:51:06 -0400

    small changes

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './footer.styles.scss'
-import { FacebookIcon, InstagramIcon } from '@mui/icons-material'
+import { Facebook, Instagram } from '@mui/icons-material'
 
 const Footer = () => {
     const year = new Date().getFullYear()
     return (
         <div className='footer'>
             {year} © Craft Me A Dream
+            <a href='https://www.facebook.com'><Facebook /></a>
+           <a href='https://www.instagram.com'> <Instagram /></a>
         </div>
     )
 }
