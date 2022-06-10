d8aa3e5aa40e719ce2e636daa9200d6bf26c01e2
Laurice Howard <lowkeylaurice@gmail.com>
2022-06-09 20:57:34 -0400

    fix react error

--- src/components/pages/cart-page/cart-page.jsx
+++ src/components/pages/cart-page/cart-page.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useContext } from 'react/cjs/react.development';
+import { useContext } from 'react';
 import { CartContext } from '../../../context/cart-context';
 import Layout from '../../shared/layout';
 import CartItem from './cart-item';
