69ee70747b0b653a793e852b7d7a441b7b6e4924
Laurice Howard <lowkeylaurice@gmail.com>
2022-06-09 23:13:56 -0400

    fixing things

--- src/components/pages/shop/shop.jsx
+++ src/components/pages/shop/shop.jsx
@@ -38,7 +38,7 @@ const Shop = () => {
         justifyContent="center" 
         sx={{ padding: isMobile ? '1rem 0' : isTablet ? '1.5rem 1.75rem 0rem 1.25rem' : '2rem 1.75rem 2rem 1.25rem', background: 'rgba(255, 216, 196, .7)'}}
         >
-        { token ? allProductsAdmin : allProducts }
+        { allProducts }
             </Grid>
            </div>
    
