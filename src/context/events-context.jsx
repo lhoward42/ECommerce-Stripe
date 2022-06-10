2936c0eafe94c1d9c003cc0363ce8e7b34dbca6f
Laurice Howard <lowkeylaurice@gmail.com>
2022-06-09 22:18:25 -0400

    json position isue

--- src/context/events-context.jsx
+++ src/context/events-context.jsx
@@ -5,79 +5,79 @@ import { format, isMatch } from 'date-fns';
 export const EventsContext = createContext();
 
 const EventsContextProvider = ({ children }) => {
-const [events, setEvents] = useState([])
-const [event, setEvent] = useState({});
-const [title, setTitle] = useState(null);
-const [imageUrl, setImageUrl] = useState(null);
-const [description, setDescription] = useState(null);
-const [date, setDate] = useState(null);
-const [startTime, setStartTime] = useState(null);
-const [endTime, setEndTime] = useState(null);
-const [location, setLocation] = useState(null);
-const [checked, setChecked] = useState(true)
+    const [events, setEvents] = useState([])
+    const [event, setEvent] = useState({});
+    const [title, setTitle] = useState(null);
+    const [imageUrl, setImageUrl] = useState(null);
+    const [description, setDescription] = useState(null);
+    const [date, setDate] = useState(null);
+    const [startTime, setStartTime] = useState(null);
+    const [endTime, setEndTime] = useState(null);
+    const [location, setLocation] = useState(null);
+    const [checked, setChecked] = useState(true)
 
-useEffect(() => {
-  fetchAllEvents();
-}, [])
+    useEffect(() => {
+        fetchAllEvents();
+    }, [])
 
 
-//MUI Functions 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-    PaperProps: {
-        style: {
-         maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-        width: 250,
+    //MUI Functions 
+    const ITEM_HEIGHT = 48;
+    const ITEM_PADDING_TOP = 8;
+    const MenuProps = {
+        PaperProps: {
+            style: {
+                maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+                width: 250,
+            },
         },
-    },
-};
-
-function getStyles(val, removeVal, theme) {
-    return {
-      fontWeight:
-        removeVal.indexOf(val) === -1
-          ? theme.typography.fontWeightRegular
-          : theme.typography.fontWeightMedium,
     };
-  }
 
-  
+    function getStyles(val, removeVal, theme) {
+        return {
+            fontWeight:
+                removeVal.indexOf(val) === -1
+                    ? theme.typography.fontWeightRegular
+                    : theme.typography.fontWeightMedium,
+        };
+    }
+
+
 
     const handleChange = (e) => {
         const { name, value, checked } = e.target;
         switch (name) {
             case 'title': {
-                setEvent({...event, title: value });
+                setEvent({ ...event, title: value });
                 break;
             }
             case 'imageUrl': {
-                setEvent({...event, imageUrl: value });
+                setEvent({ ...event, imageUrl: value });
                 break;
             }
             case 'description': {
-                setEvent({...event, description: value });
+                setEvent({ ...event, description: value });
                 break;
             }
             case 'date': {
-                setEvent({...event, date: value });
+                setEvent({ ...event, date: value });
                 break;
             }
             case 'startTime': {
-                setEvent({...event, startTime: value });
+                setEvent({ ...event, startTime: value });
                 break;
             }
             case 'endTime': {
-                setEvent({...event, endTime: value }); 
+                setEvent({ ...event, endTime: value });
                 break;
             }
             case 'location': {
-                setEvent({...event, location: value });
+                setEvent({ ...event, location: value });
                 break;
             }
             case 'hasProduct': {
                 setChecked(checked);
-                
+
                 console.log(checked);
                 break;
             }
@@ -87,30 +87,19 @@ function getStyles(val, removeVal, theme) {
     }
 
     const fetchAllEvents = async () => {
-        try{
-            // let res = await fetch(`${APIURL}/events/all`, {
-            //     method: "GET", 
-            //     headers: new Headers({
-            //         "Content-Type": "application/json",
-            //     }),
-            // });
+        try {
             fetch(`${APIURL}/events/all`, {
-                method: "GET", 
+                method: "GET",
                 headers: new Headers({
                     "Content-Type": "application/json",
                 }),
-            }).then(response => {
-                console.log('events context response ===', response);
-                return response.json();
-            }).then(async (data) => {
+            }).then(response => response.json()).then(async (data) => {
                 setEvents(data);
-                await localStorage.setItem("events", JSON.stringify(data));
                 console.log('events context data ===', data);
+                if (data) {
+                    await localStorage.setItem("events", JSON.stringify(data));
+                }
             });
-            // let data = await res.json() || {};
-            // await setEvents(data);
-            // await localStorage.setItem("events", JSON.stringify(data));
-            // console.log(data);
         } catch (err) {
             console.log(err)
         }
@@ -120,33 +109,33 @@ function getStyles(val, removeVal, theme) {
     const createEvent = async (e) => {
         e.preventDefault();
         const token = localStorage.getItem("token")
-        const eventData = {...event, hasProduct: checked };
-    
+        const eventData = { ...event, hasProduct: checked };
+
         try {
-            if (token){
-            let res = await fetch(`${APIURL}/events/new-event`, {
-                method: "POST",
-                headers: new Headers({
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                }),
-                body: JSON.stringify(eventData),
-               })  
-            let data = await res.json();
-            console.log(data);
-            alert(`${event.title} has been created`)
-            console.log("Success", eventData);
-        } else {
-            alert('Admin must be logged in to create event')
+            if (token) {
+                let res = await fetch(`${APIURL}/events/new-event`, {
+                    method: "POST",
+                    headers: new Headers({
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    }),
+                    body: JSON.stringify(eventData),
+                })
+                let data = await res.json();
+                console.log(data);
+                alert(`${event.title} has been created`)
+                console.log("Success", eventData);
+            } else {
+                alert('Admin must be logged in to create event')
+            }
+        } catch (err) {
+            console.log(err);
         }
-    } catch (err) {
-        console.log(err);
-    }
-    
+
     }
 
     //Update Event 
-    const updateEvent = async (e) =>{
+    const updateEvent = async (e) => {
         e.preventDefault();
         const token = localStorage.getItem("token");
         const eventData = {
@@ -159,8 +148,8 @@ function getStyles(val, removeVal, theme) {
             location: location,
             hasProduct: checked
         };
-        try { 
-            const res = await fetch(`${APIURL}/events/${event.id}`,{
+        try {
+            const res = await fetch(`${APIURL}/events/${event.id}`, {
                 method: 'PUT',
                 headers: new Headers({
                     "Content-Type": "application/json",
@@ -180,26 +169,26 @@ function getStyles(val, removeVal, theme) {
 
     const deleteEvent = async (event) => {
         const token = localStorage.getItem("token");
-        try{
-        const res = await fetch(
-            `${APIURL}/events/${event.id}`, {
+        try {
+            const res = await fetch(
+                `${APIURL}/events/${event.id}`, {
                 method: "DELETE",
                 headers: new Headers({
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${token}`,
-                    }),
-                }
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                }),
+            }
             );
-        const data = res.json();
-        console.log(data);
-        alert("Event successfully deleted");
-        let array = await [...events];
-        console.log(array);
-        let index = array.indexOf(event);
-        if (index !== -1){
-            array.splice(index, 1);
-            setEvents(array)
-        }
+            const data = res.json();
+            console.log(data);
+            alert("Event successfully deleted");
+            let array = await [...events];
+            console.log(array);
+            let index = array.indexOf(event);
+            if (index !== -1) {
+                array.splice(index, 1);
+                setEvents(array)
+            }
 
         } catch (err) {
             console.log(err);
@@ -208,36 +197,36 @@ function getStyles(val, removeVal, theme) {
 
     const toStandardTime = (time) => {
         time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
-  
-        if(time.length > 1){
-          console.log(time);
-          time = time.slice(1);
-          console.log(time);
-          time = time.slice(0,3);
-          console.log(time);
-          time[4] = +time[0] < 12 ? ' AM' : ' PM';
-          time[0] = +time[0] % 12 || 12;
-          
+
+        if (time.length > 1) {
+            console.log(time);
+            time = time.slice(1);
+            console.log(time);
+            time = time.slice(0, 3);
+            console.log(time);
+            time[4] = +time[0] < 12 ? ' AM' : ' PM';
+            time[0] = +time[0] % 12 || 12;
+
         }
         return time.join('')
-      }  
+    }
 
-    const contextValues = {    
+    const contextValues = {
         event,
         title,
         description,
         date,
         MenuProps,
         events,
-        startTime,        
+        startTime,
         endTime,
-        location,  
+        location,
         checked,
         imageUrl,
         deleteEvent,
         toStandardTime,
-        setImageUrl,       
-        setChecked, 
+        setImageUrl,
+        setChecked,
         setLocation,
         setStartTime,
         setEndTime,
@@ -252,10 +241,10 @@ function getStyles(val, removeVal, theme) {
         handleChange,
     }
 
-   
+
 
     return (
-        <EventsContext.Provider value={ contextValues }>
+        <EventsContext.Provider value={contextValues}>
             {
                 children
             }
--- src/context/products-context.jsx
+++ src/context/products-context.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, createContext, useState } from 'react';
 import APIURL from '../utils/environment';
 
 
- export const ProductsContext = createContext();
+export const ProductsContext = createContext();
 
- const ProductsContextProvider = ({ children }) => {
+const ProductsContextProvider = ({ children }) => {
 
     const [products, setProducts] = useState([]);
     const [product, setProduct] = useState({});
@@ -13,21 +13,21 @@ import APIURL from '../utils/environment';
     const [imageUrl, setImageUrl] = useState(null);
     const [price, setPrice] = useState(null);
     const [property, setProperty] = useState(null);
-    const [val, setVal] = useState([]); 
+    const [val, setVal] = useState([]);
     const [val2, setVal2] = useState([]);
     const [category, setCategory] = useState("");
     const [newVal, setNewVal] = useState(null);
     const [newVal2, setNewVal2] = useState(null);
     const [property2, setProperty2] = useState(null);
     const [eventName, setEventName] = useState(null);
-   
+
     const [removeVal, setRemoveVal] = useState([]);
     const [removeVal2, setRemoveVal2] = useState([]);
 
     useEffect(() => {
         fetchAllProducts()
-    }, []) 
-   
+    }, [])
+
     //MUI FUNCTIONS
 
     const ITEM_HEIGHT = 48;
@@ -35,21 +35,21 @@ import APIURL from '../utils/environment';
     const MenuProps = {
         PaperProps: {
             style: {
-             maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-            width: 250,
+                maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+                width: 250,
             },
         },
     };
 
     const getStyles = (val, removeVal, theme) => {
         return {
-          fontWeight:
-            removeVal.indexOf(val) === -1
-              ? theme.typography.fontWeightRegular
-              : theme.typography.fontWeightMedium,
+            fontWeight:
+                removeVal.indexOf(val) === -1
+                    ? theme.typography.fontWeightRegular
+                    : theme.typography.fontWeightMedium,
         };
-      }
-    
+    }
+
     //HANDLES CHANGES TO CREATE-PRODUCT.JS FORM
 
     const handleChange = (e) => {
@@ -80,7 +80,7 @@ import APIURL from '../utils/environment';
             case 'property2': {
                 setProduct({ ...product, property2: value });
                 break;
-            }          
+            }
             case 'category': {
                 setProduct({ ...product, category: value });
                 break;
@@ -92,31 +92,34 @@ import APIURL from '../utils/environment';
             default:
                 break;
         }
-        }
+    }
 
-        
+
 
     const fetchAllProducts = async () => {
-       try {
-        let res = await fetch(`${APIURL}/products/all`, { 
-            method: "GET", 
-            headers: new Headers({
-                "Content-Type": "application/json",
-            }),
-        });
-        let data = await res.json() || {};
-        await setProducts(data); 
-        if (data) { await localStorage.setItem("products", JSON.stringify(data))} ;
-        console.log(data); 
-    } catch (err) {
-        console.log(err);
-    } 
+        try {
+            fetch(`${APIURL}/products/all`, {
+                method: "GET",
+                headers: new Headers({
+                    "Content-Type": "application/json",
+                }),
+            }).then(response => response.json()).then(async (data) => {
+                setProducts(data);
+                console.log('products context data ===', data);
+                if (data) {
+                    await localStorage.setItem("products", JSON.stringify(data));
+                };
+                console.log(data);
+            });
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     //EVENT FUNCTION CREATES A NEW PRODUCT
 
     const createNewProduct = async (e) => {
-       e.preventDefault()
+        e.preventDefault()
         const token = localStorage.getItem("token");
         const productData = {
             title: product.title,
@@ -133,25 +136,25 @@ import APIURL from '../utils/environment';
 
         console.log(productData);
         try {
-        if(token){
-            let res = await fetch(`${APIURL}/products/new-product`, {
-            method: "POST",
-            headers: new Headers({
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`,
-            }),
-            body: JSON.stringify(productData),
-           })  
-        let data = await res.json();
-        console.log(data);
-        if (data) { alert('new product created') }
-        } else {
-            alert('Admin must be logged in to create a product')
-        }
+            if (token) {
+                let res = await fetch(`${APIURL}/products/new-product`, {
+                    method: "POST",
+                    headers: new Headers({
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    }),
+                    body: JSON.stringify(productData),
+                })
+                let data = await res.json();
+                console.log(data);
+                if (data) { alert('new product created') }
+            } else {
+                alert('Admin must be logged in to create a product')
+            }
         } catch (err) {
             console.log(err);
-            
-        } 
+
+        }
     }
 
     //EVENT FUNCTION UPDATES THE PRODUCT
@@ -174,61 +177,61 @@ import APIURL from '../utils/environment';
         };
 
         try {
-            if (token){
-            let res = await fetch(`${APIURL}/products/${product.id}`, {
-                method: 'PUT',
-                headers: new Headers({
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                }),
-                body: JSON.stringify(productData),
-            })
-            let data = await res.json();
-            console.log(data);
-            alert('product successfully updated');
-            setRemoveVal([])
-        } else {
-            alert('Admin must be logged in to update product');
-        }
+            if (token) {
+                let res = await fetch(`${APIURL}/products/${product.id}`, {
+                    method: 'PUT',
+                    headers: new Headers({
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    }),
+                    body: JSON.stringify(productData),
+                })
+                let data = await res.json();
+                console.log(data);
+                alert('product successfully updated');
+                setRemoveVal([])
+            } else {
+                alert('Admin must be logged in to update product');
+            }
         } catch (err) {
-            console.log( "Product did not update", err )
+            console.log("Product did not update", err)
             alert('product could not be updated, please check fields and try again')
         }
-        
+
     }
 
     //DELETE PRODUCT FROM STORE
 
-    const deleteProduct = async (product) => { 
+    const deleteProduct = async (product) => {
         const token = localStorage.getItem("token");
-        
-        try{
-            if(token){
-            const res = await fetch( 
-                `${APIURL}/products/${product.id}`, {
+
+        try {
+            if (token) {
+                const res = await fetch(
+                    `${APIURL}/products/${product.id}`, {
                     method: "DELETE",
                     headers: new Headers({
                         "Content-Type": "application/json",
                         Authorization: `Bearer ${token}`,
                     }),
                 }
-            );
-            const data = res.json();
-            console.log(data);
-            alert("product successfully deleted");
-            let array = await [...products];
-            let index = array.indexOf(product);
-            if (index !== -1){
-                array.splice(index, 1);
-                setProducts(array);
-            };
+                );
+                const data = res.json();
+                console.log(data);
+                alert("product successfully deleted");
+                let array = await [...products];
+                let index = array.indexOf(product);
+                if (index !== -1) {
+                    array.splice(index, 1);
+                    setProducts(array);
+                };
             } else {
                 alert('Admin must be logged in to delete product');
             }
 
         } catch (err) {
             console.log(err);
-            
+
         }
     }
 
@@ -243,10 +246,10 @@ import APIURL from '../utils/environment';
         })
 
 
-        console.log({newArray});
-            // setVal(val.length > 1 ? newArray : [])
-            setVal(newArray)
-            setRemoveVal([])
+        console.log({ newArray });
+        // setVal(val.length > 1 ? newArray : [])
+        setVal(newArray)
+        setRemoveVal([])
         //     console.log(e);
 
     }
@@ -259,23 +262,23 @@ import APIURL from '../utils/environment';
 
     const handleChangeSelect = (event) => {
         const {
-          target: { value },
+            target: { value },
         } = event;
         setRemoveVal(
-          // On autofill we get a stringified value.
-          typeof value === 'string' ? value.split(',') : value,
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value,
         );
-      };
+    };
 
     //EVENT FUNCTIONS FOR SECOND SET OF PRODUCT ATTRIBUTES
 
     const handleRemoveVal2 = (e) => {
         e.stopPropagation();
         e.preventDefault();
-        const newArray = val2.filter(element => 
+        const newArray = val2.filter(element =>
             !removeVal2.includes(element))
-            console.log(newArray);
-            setVal2(newArray)
+        console.log(newArray);
+        setVal2(newArray)
         //    const removedIndexItem =  val2.findIndex(element => 
         //         removeVal2.includes(element))
         //      setRemoveVal2(removedIndexItem!== -1 ? removeVal2[removeVal2.length-1]:[])
@@ -283,13 +286,13 @@ import APIURL from '../utils/environment';
 
     const handleChangeSelect2 = (event) => {
         const {
-          target: { value },
+            target: { value },
         } = event;
         setRemoveVal2(
-          // On autofill we get a stringified value.
-          typeof value === 'string' ? value.split(',') : value,
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value,
         );
-      };
+    };
 
     const handleInputChangeVal2 = (e) => {
         const { value } = e.target;
@@ -301,74 +304,74 @@ import APIURL from '../utils/environment';
         e.stopPropagation();
         e.preventDefault();
         const { options } = e.target;
-       
+
         const newValues = [...options]
-        .filter(option => option.selected)
-        .map(x => x.value);
+            .filter(option => option.selected)
+            .map(x => x.value);
         console.log("New Values", newValues, "event target", e.target);
-       
-        const removed = val2.filter(element => 
+
+        const removed = val2.filter(element =>
             newValues.includes(element)
         )
         setRemoveVal2(removed)
         console.log(removed)
     }
-    
-    
-    
 
-const contextValues = {
-    products,
-    product,
-    removeVal,
-    removeVal2,
-    title,
-    description,
-    imageUrl,
-    price,
-    property,
-    val,
-    property2, 
-    val2,
-    eventName,
-    newVal,
-    newVal2,
-    MenuProps,
-    category,
-    setCategory,
-    setEventName,
-    getStyles,
-    handleChange,
-    setNewVal,
-    handleChangeVal2,
-    handleInputChangeVal,
-    handleRemoveVal,
-    handleRemoveVal2,
-    fetchAllProducts,
-    createNewProduct,
-    updateProduct,
-    setProduct,
-    setTitle,
-    setDescription,
-    deleteProduct,
-    handleChangeSelect,
-    handleChangeSelect2,
-    setPrice,
-    setImageUrl,
-    setVal,
-    setProperty,
-    setProperty2,
-    setVal2, 
-    setRemoveVal,
-    setRemoveVal2,
-    handleInputChangeVal2
 
-}
-    
+
+
+    const contextValues = {
+        products,
+        product,
+        removeVal,
+        removeVal2,
+        title,
+        description,
+        imageUrl,
+        price,
+        property,
+        val,
+        property2,
+        val2,
+        eventName,
+        newVal,
+        newVal2,
+        MenuProps,
+        category,
+        setCategory,
+        setEventName,
+        getStyles,
+        handleChange,
+        setNewVal,
+        handleChangeVal2,
+        handleInputChangeVal,
+        handleRemoveVal,
+        handleRemoveVal2,
+        fetchAllProducts,
+        createNewProduct,
+        updateProduct,
+        setProduct,
+        setTitle,
+        setDescription,
+        deleteProduct,
+        handleChangeSelect,
+        handleChangeSelect2,
+        setPrice,
+        setImageUrl,
+        setVal,
+        setProperty,
+        setProperty2,
+        setVal2,
+        setRemoveVal,
+        setRemoveVal2,
+        handleInputChangeVal2
+
+    }
+
     return (
-        <ProductsContext.Provider value={ contextValues }>
+        <ProductsContext.Provider value={contextValues}>
             {
-            children
+                children
             }
         </ProductsContext.Provider>
     )
