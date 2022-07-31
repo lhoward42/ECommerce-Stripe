203bf1451ea42602eb62fe5391aa16e621cb2af1
Laurice Howard <lowkeylaurice@gmail.com>
2022-07-31 12:47:26 -0400

    this should look good now

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -31,7 +31,7 @@ const Hero = () => {
         
                         // id='btn-white-outline' 
                         sx={{opacity: '95%', width: '25%', padding: '1rem', backgroundColor: 'rgba(64,255,249,.9)' }} > */}
-                       <Link className="shop-link" style={{ opacity: '95%', width: '25%', padding: '1.1rem 2rem', backgroundColor: 'rgba(64,255,249,.9)', fontSize: '2rem', fontWeight: 'bold', borderRadius: '4px' }} to={`/shop`}> SHOP NOW </Link>
+                       <Link className="shop-link" style={{ opacity: '97%', width: '25%', padding: '1.1rem 2rem', backgroundColor: 'rgba(64,255,249,.9)', fontSize: '2rem', fontWeight: 'bold', borderRadius: '4px' }} to={`/shop`}> SHOP NOW </Link>
                     {/* </Button> */}
                 </div>
             </div>
@@ -48,7 +48,7 @@ const Hero = () => {
                    <span className="title-word title-word-4">Occasion </span> */}
                </h1></div>
                <div className="shop-now-btn">
-                   <Button color='secondary' className="shop-now-btn" variant='contained'  sx={{opacity: '95%', backgroundColor: 'rgba(64,255,249,.83)' }} >
+                   <Button color='secondary' className="shop-now-btn" variant='contained'  sx={{opacity: '97%', backgroundColor: 'rgba(64,255,249,.83)' }} >
                       <Link style={{ fontSize: '11px'}} to={`/shop`}> SHOP NOW </Link>
                    </Button>
                </div>
